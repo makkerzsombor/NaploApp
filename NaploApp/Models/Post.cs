484aa2b8dd0ab825ad0458ab0cce0b424332a135
makkerzsombor <makker.zsombor@gmail.com>
2025-11-03 20:52:17 +0100

    model class

--- NaploApp/Models/Post.cs
+++ NaploApp/Models/Post.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NaploApp.Models
 {
-    internal class Post
+    public partial class Post : ObservableObject
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [ObservableProperty]
+        private string description;
+
+        [ObservableProperty]
+        private DateTime date;
+
+        // képnek
+        [ObservableProperty]
+        private string imagestring;
     }
 }
